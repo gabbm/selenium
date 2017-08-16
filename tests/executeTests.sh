9c6ecaa41a21ea64ee0b1ee15c5b4dbfdc40777a
Gab <gbermume@gmail.com>
2017-08-16 10:49:29 +0200

    Added Automatic RepteEmpren tests execution

--- tests/executeTests.sh
+++ tests/executeTests.sh
@@ -27,3 +27,27 @@ else
 fi
 cat /opt/selenium/results/caixaimpulse/caixaimpulse-test-results-$current_time.log | mail -s "$subject" bustia.fundacio@vass.es
 
+echo "Accessing RepteEmpren tests folder"
+cd /opt/selenium/tests/repteempren
+
+echo "If Java classes are not compiled, compile them"
+if [ -n "$(ls -l *.class | wc -l)" ]
+then 
+	echo "Compiling java classes"
+	javac -classpath ".:/opt/selenium/selenium-java-2.53.1/selenium-2.53.1/*:/opt/selenium/selenium-java-2.53.1/selenium-2.53.1/libs/*" TestRepteEmprenRunner.java
+fi 
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+
+echo "Executing RepteEmpren tests"
+java -cp ".:/opt/selenium/selenium-java-2.53.1/selenium-2.53.1/*:/opt/selenium/selenium-java-2.53.1/selenium-2.53.1/libs/*" TestRepteEmprenRunner > /opt/selenium/results/repteempren/repteempren-test-results-$current_time.log
+echo "RepteEmpren tests executed"
+
+echo "Sending email"
+if grep -q "Test successful ? false" "/opt/selenium/results/repteempren/repteempren-test-results-$current_time.log"; then
+	subject="[KO] Tests RepteEmpren $current_time"
+else
+	subject="[OK] Tests RepteEmpren $current_time"
+fi
+cat /opt/selenium/results/repteempren/repteempren-test-results-$current_time.log | mail -s "$subject" bustia.fundacio@vass.es
+
